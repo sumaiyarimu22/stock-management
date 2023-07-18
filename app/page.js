d890ab470d70c9cda452ce90ea508a55ad22c778
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-07-18 21:48:56 +0600

    search dropdow added

--- app/page.js
+++ app/page.js
@@ -6,6 +6,23 @@ const HomePAge = () => {
   const [productForm, setProductForm] = useState({});
   const [products, setProducts] = useState([]);
   const [alert, setAlert] = useState("");
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const [dropdown, setDropdown] = useState([
+    {
+      _id: "64b57a9b13a384fe1ccf0125",
+      slug: "pant",
+      quantity: "7",
+      price: "467",
+    },
+    {
+      _id: "64b5800613a384fe1ccf0126",
+      slug: "Tshirt",
+      quantity: "34",
+      price: "2334",
+    },
+  ]);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -42,17 +59,33 @@ const HomePAge = () => {
   const handleChange = (e) => {
     setProductForm({ ...productForm, [e.target.name]: e.target.value });
   };
+
+  const onDropdownEdit = async (e) => {
+    setQuery(e.target.value);
+    if (!loading) {
+      setLoading(true);
+      setDropdown([]);
+      const response = await fetch("/api/search?query=" + query);
+      let rjson = await response.json();
+      setDropdown(rjson.products);
+      setLoading(false);
+    }
+  };
   return (
     <>
       <Header />
       <div className="text-green-700 text-center">{alert}</div>
-      {/* DISPLAY CURRENT STOCK */}
-      <div className="container mx-auto bg-blue-200 px-5  my-10 md:p-10 rounded-lg">
+
+      <div className="container mx-auto  px-5  my-10 md:p-10 rounded-lg">
         {/* SEARCH A PRODUCT */}
-        <div className="container mx-auto  px-5 md:px-0 mt-4 py-8">
+        <div className="container mx-auto relative px-5 md:px-0 mt-4 py-8">
           <h1 className="text-2xl font-semibold mb-2">Search a Product</h1>
           <div className="flex items-center">
             <input
+              onBlur={() => {
+                setDropdown([]);
+              }}
+              onChange={onDropdownEdit}
               type="text"
               placeholder="Enter product name"
               className="px-4 py-2 w-full border border-gray-300 rounded-l-md outline-none"
@@ -63,6 +96,53 @@ const HomePAge = () => {
               <option value="5">5</option>
               <option value="10">10</option>
             </select>
+          </div>
+
+          {/* LOADING SPINNER */}
+          {loading && (
+            <div className="flex justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20px"
+                height="20px"
+                viewBox="0 0 40 40"
+                stroke="#000"
+              >
+                <circle
+                  cx="20"
+                  cy="20"
+                  r="18"
+                  fill="none"
+                  strokeWidth="4"
+                  strokeDasharray="90 60"
+                >
+                  <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    from="0 20 20"
+                    to="360 20 20"
+                    dur="1.5s"
+                    repeatCount="indefinite"
+                  />
+                </circle>
+              </svg>
+            </div>
+          )}
+
+          {/* DROP DOWN CONTAINER */}
+          <div className="dropContainer absolute bg-blue-100 mt-1 container mx-auto w-[80vw] sm:w-full rounded-md">
+            {dropdown.map((item, i) => {
+              return (
+                <div
+                  className="flex justify-between  my-2 px-10 gap-5 border-b "
+                  key={i}
+                >
+                  <span className=""> {item.slug}</span>
+                  <span className="">{item.price}</span>
+                  <span className="">{item.quantity}</span>
+                </div>
+              );
+            })}
           </div>
         </div>
 
@@ -112,7 +192,7 @@ const HomePAge = () => {
               Price
             </label>
             <input
-              value={productForm?.priceForm || ""}
+              value={productForm?.price || ""}
               name="price"
               onChange={handleChange}
               type="number"
@@ -130,6 +210,8 @@ const HomePAge = () => {
             Add Product
           </button>
         </form>
+
+        {/* CURRENT STOCK */}
         <h1 className="text-2xl font-semibold pt-8 ">Current Stock</h1>
         <table className="table-auto w-full mt-4">
           <thead>
